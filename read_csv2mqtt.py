90e89d620adfa0f9a77ef237abd6b43bc27a2c24
Juan Pérez <133648979+JuanPerez07@users.noreply.github.com>
2024-11-24 14:47:12 +0100

    publisher a 8 Hz simulado

--- read_csv2mqtt.py
+++ read_csv2mqtt.py
@@ -10,7 +10,8 @@ import time
 import pandas as pd
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-
+# frecuencia de publicacion / lectura datos
+FREQ_PUB = 8 # Hz
 # Class to publish data to the broker HiveMQ
 class Publisher:
     """
@@ -98,7 +99,6 @@ if __name__ == "__main__":
         ext = row["ext_value"]
         time.sleep(0.1)
     """
-    freq = 4 # Hz
     # Crear un objeto Publisher y conectarse al broker
     pub = Publisher(Publisher.server_mqtt, Publisher.puerto_mqtt, "esp32_emg", "Servicios25")
     if pub.connect():
@@ -107,7 +107,7 @@ if __name__ == "__main__":
             pub.publish("/timestamp", row["tiempo"])
             pub.publish("/emg/flexion", row["flex_value"])
             pub.publish("/emg/extension", row["ext_value"])
-            time.sleep(1/freq)  # Pausa de 1 segundo entre mensajes
+            time.sleep(1/FREQ_PUB)  # intervalo entre publicaciones
         # Desconectar al finalizar
         pub.disconnect()
 
